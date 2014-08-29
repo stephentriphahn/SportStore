c23104bda5a75da9062c9491ec1739ff9ee48365
steve <stephentriphahn@yahoo.com>
2014-08-28 21:20:43 -0400

    add to cart buttons, functioning cart math

--- app/js/controllers/productListControllers.js
+++ app/js/controllers/productListControllers.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
 	.constant("productListActiveClass", "btn-primary")
 	.constant("productListPageCount", 3)
-	.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+	.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
 
 	var selectedCategory = null;
 	
@@ -32,4 +32,7 @@ angular.module("sportsStore")
 		return $scope.selectedPage == page ? productListActiveClass : "";
 	}
 
+	$scope.addProductToCart = function(product) {
+		cart.addProduct(product.id, product.name, product.price);
+	}
 });
